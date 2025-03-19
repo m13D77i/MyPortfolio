9959b21104541a9ea1de0751ce0447c391104996
m13D77i <m13d77i@gmail.com>
2025-03-19 16:28:34 +0330

    add test for custom user model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            username = "user1",
+            password = "pass-123",
+            email = "user-1@gmail.com"
+        )
+
+    def test_save_user_object_to_model(self):
+        self.assertEqual(self.user.username, "user1")
+        self.assertEqual(self.user.email, "user-1@gmail.com")
