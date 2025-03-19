b033b687388d9e86852f74cf705eb5c5892103c5
m13D77i <m13d77i@gmail.com>
2025-03-19 17:30:51 +0330

    add test for the contact page to project

--- pages/tests.py
+++ pages/tests.py
@@ -4,6 +4,11 @@ from django.shortcuts import reverse
 
 class PagesTests(TestCase):
 
+    # test for home page
+
+    def test_home_page_error_404(self):
+        response = self.client.get(reverse("home"))
+        self.assertNotEqual(response.status_code, 404)
 
     def test_home_page_url(self):
         response = self.client.get('')
@@ -20,6 +25,29 @@ class PagesTests(TestCase):
     def test_home_page_template(self):
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "pages/home.html")
+
+
+    # test for contact page
+
+    def test_contact_page_url(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_url_by_name(self):
+        response = self.client.get(reverse("contact"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_content(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(response, "Contact me")
+
+    def test_contact_page_template(self):
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response, "pages/contact.html")
+
+    def test_contact_page_error_404(self):
+        response = self.client.get(reverse("contact"))
+        self.assertNotEqual(response.status_code, 404)
 
 
 
