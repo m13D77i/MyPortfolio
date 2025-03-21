71dd42ad71565704fe15de17539dce13b76a24b3
m13D77i <m13d77i@gmail.com>
2025-03-21 11:41:47 +0330

    install environs package and config / edit html files

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-krgknoru1!fwxh#gv-%)m1*dgi^$j%6i33-p*+==81)jlqmzj4'
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = []
 
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    fullname = models.CharField(max_length=50, verbose_name="full name")
+    email = models.EmailField(unique=True, verbose_name="email address")
+    subject = models.CharField(max_length=250, verbose_name="subject")
+    description = models.TextField(verbose_name="description")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="datatime created")
+
+
+    def __str__(self):
+        return self.fullname
